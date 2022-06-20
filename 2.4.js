f6c1d283f9baaf3e2ddd8cbb92c1f050f2939277
DashaTep <95724213+DashaTep@users.noreply.github.com>
2022-06-20 16:16:14 +0300

    2.4

---
+++ 2.4.js
@@ -0,0 +1,10 @@
+let admin;
+let name;
+name="Джон";
+admin=name;
+alert(admin)
+
+let OutPlanet;
+let currentUser;
+
+//Для birthday можно использовать заглавные буквы, для age не стоит, потому что она будет меняться по ходу времени
