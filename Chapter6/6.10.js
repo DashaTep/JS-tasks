bb71a594adc0531e540041d5ccc6efd574c3c146
DashaTep <95724213+DashaTep@users.noreply.github.com>
2022-06-23 15:41:57 +0300

    6.10

---
+++ Chapter6/6.10.js
@@ -0,0 +1,37 @@
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+  }
+  
+  let user = {
+    name: 'Вася',
+  
+    loginOk() {
+      alert(`${this.name} logged in`);
+    },
+  
+    loginFail() {
+      alert(`${this.name} failed to log in`);
+    },
+  
+  };
+  
+  askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+//------------------------------------------------------------------------------------------------------
+
+  function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
+  }
+  
+  let user1 = {
+    name: 'John',
+  
+    login(result) {
+      alert( this.name + (result ? ' logged in' : ' failed to log in') );
+    }
+  };
+  
+  askPassword(user1.login.bind(user, true), user1.login.bind(user, false)); 
